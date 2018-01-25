c8f2e11afcf015d585961502ed4f4a0696f8d903
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-01-24 23:15:43 -0600

    Se agrega ejemplo de arrays

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,18 @@
 public class Principal {
 
     public static void main(String[] args){
-        
+        // Estructura no dinamica
+        int[] arreglo = new int[7];
+
+        arreglo[0] = 20;
+        arreglo[1] = 35;
+        arreglo[2] = -14;
+        arreglo[3] = 7;
+        arreglo[4] = 12;
+        arreglo[5] = 23;
+        arreglo[6] = 232;
+
+        for(int x = 0; x < arreglo.length; x++)
+            System.out.println(arreglo[x]);
     }
 }
